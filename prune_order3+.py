7199c414786742d7d88f5c3d768f77b81e427b54
Josh Ogden <77122008+J-Ogden99@users.noreply.github.com>
2023-03-09 14:36:20 -0700

    Finish function that will take network and catchment file, prune order ones that feed into higher order streams, and join those polygons with their higher-order siblings.

--- prune_order3+.py
+++ prune_order3+.py
@@ -1,115 +1,81 @@
 import json
-
 import geopandas as gpd
-
-from AdjoinUpdown import NpEncoder
-
-
-# class NpEncoder(json.JSONEncoder):
-#     def default(self, obj):
-#         if isinstance(obj, np.integer):
-#             return int(obj)
-#         if isinstance(obj, np.floating):
-#             return float(obj)
-#         if isinstance(obj, np.ndarray):
-#             return obj.tolist()
-#         return super(NpEncoder, self).default(obj)
+import numpy as np
+import pandas as pd
 
 
-def get_downstream_order(x, network_gdf: gpd.GeoDataFrame):
-    ord = 0
-    if x != -1:
-        orders = network_gdf.loc[network_gdf[riv_id_col] == x, order_col].values
-        if len(orders) > 0:
-            ord = orders[0]
-        # print(ord)
-    return ord
-
-
-def merge_catchments(row, network_gdf: gpd.GeoDataFrame, upstream_dict: dict):
-    print(row)
-    print(row[riv_id_col])
-    ids_to_merge = [row[ds_id_col], ] + list(upstream_dict[str(row[riv_id_col])])
-    return network_gdf.loc[network_gdf[riv_id_col].isin(ids_to_merge)].dissolve()
-
-
-def prune_network(network_gdf: gpd.GeoDataFrame, upstream_dict: dict) -> gpd.GeoDataFrame:
+def prune_stream_network(streams_gdf, polygons_gdf):
     """
-    Finds all order ones that feed into a larger order river (without forming an order 2 with another river), joins
-    their area down onto the larger river, and drops their river segment from the network.
-
+    Chat GPT-generated function
     Args:
-        network_gdf: geodataframe containing the stream network
-        upstream_dict: dictionary containing all upstream stream ids for each stream segment
+        streams_gdf:
+        polygons_gdf:
 
-    Returns: pruned network geodataframe
+    Returns:
 
     """
-    network_gdf['_next_down_order'] = network_gdf[ds_id_col].apply(get_downstream_order, network_gdf=network_gdf)
+    # Create subsets of order 1 and higher-order segments
+    order_1 = streams_gdf.loc[streams_gdf[order_col] == 1]
+    higher_order = streams_gdf.loc[streams_gdf[order_col] >= 3]
 
-    network_gdf.loc[(network_gdf[order_col] == 1) & (network_gdf['_next_down_order'] >= 3)] = \
-        network_gdf.loc[(network_gdf[order_col] == 1) & (network_gdf['_next_down_order'] >= 3)] \
-        .apply(merge_catchments, network_gdf=network_gdf, upstream_dict=upstream_dict, axis=1)
+    # Add the next down ids field onto the polygons gdf
+    polygons_gdf_new = polygons_gdf.merge(streams_gdf[[riv_id_col, ds_id_col, order_col]],
+                                      left_on=chmt_riv_id_col,
+                                      right_on=riv_id_col)
+    print(polygons_gdf_new)
+    # Create list of downstream IDs for higher-order segments
+    # downstream_ids = list(higher_order[ds_id_col])
+    # Create list of upstream IDs for higher-order segments
+    upstream_ids = list(higher_order[ds_id_col])
 
-    network_gdf.drop(network_gdf[(network_gdf[order_col] == 1) & (network_gdf['_next_down_order'] >= 3)].index,
-                     inplace=True)
-    return network_gdf
+    # Find order 1 segments with higher order streams as their direct children
+    pruned_order_1 = order_1.loc[order_1[ds_id_col].isin(higher_order[riv_id_col].tolist())]
+    pruned_order_1_ids = pruned_order_1[riv_id_col].to_list()
+    higher_order_sibling_ids = higher_order.loc[
+        higher_order[ds_id_col].isin(pruned_order_1[ds_id_col]), riv_id_col].tolist()
 
+    # Get the polygons that need to be pruned and their siblings and dissolve them together by their downstream id to
+    # find siblings
+    polygons_to_merge = polygons_gdf_new.loc[
+        polygons_gdf_new[riv_id_col].isin(higher_order_sibling_ids + pruned_order_1_ids)].sort_values(by=order_col)
 
-def prune_stream(stream_id: int, upstream_list: list, streams_to_merge: list):
-    """
-    Removes all streams upstream of a given segment whose order is less than (<) the given max_order
-    Args:
-        stream_id: unique id of the stream to prune
-        upstream_list: list produced using AdjointCatchments that contains all upstream segment ids
+    dissolved_polygons = polygons_to_merge.dissolve(by=ds_id_col, aggfunc='last')
+    merged_polygons = gpd.GeoDataFrame(pd.concat(
+        [polygons_gdf.loc[~polygons_gdf[chmt_riv_id_col].isin(higher_order_sibling_ids + pruned_order_1_ids)],
+         dissolved_polygons[polygons_gdf.columns]]))
 
-    Returns: dictionary attaching a list of the pruned ids to the stream_id so that the catchment areas corresponding to
-    those regions can be merged down
-    """
-    upstream_rows = gdf[gdf[ds_id_col] == stream_id]
-    print(upstream_rows)
-    if 1 in upstream_rows[order_col]:
-        upstream_order_1_id = int(upstream_rows[upstream_rows[order_col] == 1][riv_id_col])
-        rows_to_delete = gdf[gdf[riv_id_col].isin(upstream_list[str(upstream_order_1_id)])]
-        gdf.drop(index=rows_to_delete.index, axis=0)
-        streams_to_merge.append({stream_id: rows_to_delete[riv_id_col].to_list()})
-        pbar.update(1)
-    return streams_to_merge
+    print(dissolved_polygons)
 
+    # Prune the unneccesary order_ones
+    pruned_network = streams_gdf[~streams_gdf[riv_id_col].isin(pruned_order_1_ids)]
+
+    return pruned_network, merged_polygons
+
+pd.options.display.max_columns = 20
+pd.options.display.width = 400
 
 region = 'japan'
 all_order_json = f'output_jsons/{region}_allorders.json'
 network_gpkg = f'Japan/TDX_streamnet_4020034510_01.shp'
-output_gpkg_name = f'pruned_{region}.gpkg'
+chmt_gpkg = f'Japan/TDX_streamreach_basins_4020034510_01.gpkg'
+output_gpkg_name = f'output_gpkgs/pruned_network_{region}.gpkg'
+output_gpkg_chmt_name = f'output_gpkgs/dissolved_catchments_{region}.gpkg'
 output_json_name = f'output_jsons/{region}_streams_to_prune.json'
 
 max_order = 2
 riv_id_col = 'LINKNO'
 ds_id_col = 'DSLINKNO'
 order_col = 'strmOrder'
+chmt_riv_id_col = 'streamID'
 
 if __name__ == '__main__':
-    with open(all_order_json) as f:
-        allorders_dict = json.load(f)
+    pd.options.display.max_columns = 20
+    pd.options.display.width = 400
 
-    gdf = gpd.read_file(network_gpkg)
+    streams_gdf = gpd.read_file(network_gpkg)
+    chmt_gdf = gpd.read_file(chmt_gpkg)
 
-    # order_3up = stream_net[stream_net[order_col] > 2]
-    order_3up_ids = gdf[gdf[order_col] > 2][riv_id_col].to_list()
+    pruned_network, dissolved_polygons = prune_stream_network(streams_gdf, chmt_gdf)
 
-    # streams_to_merge = []
-    # print()
-    gdf = prune_network(gdf, allorders_dict)
-
-    # rivid = order_3up_ids[100]
-    # prune_stream(rivid, allorders_dict[str(rivid)], streams_to_merge)
-    # for rivid in order_3up_ids:
-    #     streams_to_merge = prune_stream(rivid, allorders_dict[str(rivid)], streams_to_merge)
-    # with Pool() as p:
-    #     gdf, streams_to_merge = p.starmap(prune_stream, [(rivid, max_order, allorders_dict[str(rivid)], stream_net, streams_to_merge) for rivid in order_3up_ids])
-
-    # print(streams_to_merge)
-    # with open(output_json_name, "w") as f:
-    #     json.dump(streams_to_merge, f, cls=NpEncoder)
-
-    gdf.to_file(output_gpkg_name, driver="GPKG")
+    dissolved_polygons.to_file(output_gpkg_chmt_name, driver="GPKG")
+    pruned_network.to_file(output_gpkg_name, driver="GPKG")
