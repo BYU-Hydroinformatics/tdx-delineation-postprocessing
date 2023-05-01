648e131b48a41d193790a4b6de9cfe1b2197bd3c
RickytheGuy <106771408+RickytheGuy@users.noreply.github.com>
2023-05-01 11:20:59 -0600

    Update Convert_Flowlines_to_CSV_for_CF.py

--- Convert_Flowlines_to_CSV_for_CF.py
+++ Convert_Flowlines_to_CSV_for_CF.py
@@ -10,14 +10,15 @@ import pandas as pd
 import geopandas as gpd
 import os
 
-def CreateComidLatLonZ(network: gpd.GeoDataframe, out_dir: str, id_field: str) -> None:
+def CreateComidLatLonZ(network: str, out_dir: str, id_field: str) -> None:
     """
-    Create a comid_lat_lon_z csv in output directory, based on an input drainage line network that has been read in as a Geopandas Geodataframe.
+    Create a comid_lat_lon_z csv in output directory, based on an input drainage line network.
     id_field refers to the ID field that represents the unique id for each stream segment in your drainage line
     
     Note: We assume that the value "z", or the elevation, is not present in the drainage line, and so it is set to 0.
     """
     # Ensure that the drainage line is sorted
+    network = gpd.read_file(network)
     network.sort_values(id_field, inplace=True)
     gdf = gpd.GeoDataFrame.copy(network)
     gdf['lat'] = network.geometry.apply(lambda geom: geom.xy[1][0])
@@ -28,3 +29,4 @@ def CreateComidLatLonZ(network: gpd.GeoDataframe, out_dir: str, id_field: str) -
             "z": 0}
     
     pd.DataFrame(data).to_csv(os.path.join(out_dir, "comid_lat_lon_z.csv"), index=False, header=True)
+   
